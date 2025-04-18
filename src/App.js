aeb8cfcd7ddb9fb3086ad280e96e82a5901be6a9
SirBlessing <olaniyiolawale014@gmail.com>
2025-04-18 15:15:14 +0100

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
 import './App.css';
 import Layout from './Layout';
 import NoLayout from './NoLayout';
-import Homepage from './pages/Homepage'
-import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
-import  DonorPage from './pages/DonorPage';
+import Homepage from './pages/Homepage';
+import DonorPage from './pages/DonorPage';
 import Blogs from './Blogs';
-import Reciepientpage from './pages//Reciepientpage'
-import  Hospitaldash from "./pages/Hospitaldash";
-import Aboutuspage from './pages/Aboutuspage'
+import Reciepientpage from './pages/Reciepientpage';
+import Hospitaldash from './pages/Hospitaldash';
+import Aboutuspage from './pages/Aboutuspage';
+
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename='/DonorConnect-Web'>
-	 <Router>
-        <Routes>
-          {/* Routes that should have Navbar and Footer */}
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Homepage />} />
-            <Route path="/donorPage" element={<DonorPage />} />
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/reciepientpage" element={<Reciepientpage />} />
-            <Route path="/hospitaldash" element={<Hospitaldash />} />
-            <Route path="/aboutuspage" element={<Aboutuspage />} />
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/reciepientpage" element={<Reciepientpage />} />
-            
-          </Route>
+      <Routes>
+        {/* Routes with Navbar and Footer */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/donorPage" element={<DonorPage />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/reciepientpage" element={<Reciepientpage />} />
+          <Route path="/hospitaldash" element={<Hospitaldash />} />
+          <Route path="/aboutuspage" element={<Aboutuspage />} />
+        </Route>
 
-          {/* Routes that should NOT have Navbar and Footer */}
-          <Route element={<NoLayout />}>
-            
-          </Route>
-        </Routes>
-      </Router>
-      </BrowserRouter>
-	</div>
+        {/* Routes without Navbar and Footer */}
+        <Route element={<NoLayout />}>
+          {/* You can add routes here that need NoLayout */}
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
